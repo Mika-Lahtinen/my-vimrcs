5ec87f79bdc7562907e11e754a952b7aca8f93d7
Kurtis Moxley <image1224@outlook.com>
2023-03-31 00:12:29 +0800

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+cd ~/.my-vimrcs
+
+echo '
+set runtimepath+=~/.my-vimrcs
+
+source ~/.my-vimrcs/vimrcs/basic.vim
+source ~/.my-vimrcs/vimrcs/plugins.vim
+source ~/.my-vimrcs/vimrcs/plugins_settings.vim
+' > ~/.vimrc
